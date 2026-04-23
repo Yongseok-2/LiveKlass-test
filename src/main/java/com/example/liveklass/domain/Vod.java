87e244891cb67a3fb899a4b9d087ff7b6705d2cb
Yongseok-2 <8465867@naver.com>
2026-04-24 00:06:43 +0900

    feat: data.sql 추가

--- src/main/java/com/example/liveklass/domain/Vod.java
+++ src/main/java/com/example/liveklass/domain/Vod.java
@@ -4,10 +4,14 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE vod SET is_deleted = true WHERE id = ?")
+@SQLRestriction("is_deleted = false")
 public class Vod extends BaseEntity {
 
     @Id
@@ -25,6 +29,9 @@ public class Vod extends BaseEntity {
 
     @Column(nullable = false)
     private long viewCount = 0;
+
+    @Column(nullable = false)
+    private boolean isDeleted = false;
 
     public void increaseViewCount() {
         this.viewCount++;
